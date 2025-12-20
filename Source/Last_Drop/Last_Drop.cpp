d636db20e8aada4ae58efcd5765c718db5320e8c
btjdco <btjdco@naver.com>
2025-12-21 00:14:57 +0900

    Init

---
+++ Source/Last_Drop/Last_Drop.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Last_Drop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Last_Drop, "Last_Drop" );
