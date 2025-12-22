6114cc760e3043703a016a051f9c5c4d2bae2e07
btjdco <btjdco@naver.com>
2025-12-22 16:06:40 +0900

    InitMultiplayReady

---
+++ Source/Last_Drop/Public/CBL_Math.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CBL_Math.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAST_DROP_API UCBL_Math : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	UFUNCTION(BlueprintPure, Category = "Create_Game")
+	static FString GenerateVoidJumperCode();
+
+	UFUNCTION(BlueprintCallable, Category = "Create_Game")
+	static FString CreateUniqueRoomCode();
+
+	UFUNCTION(BlueprintCallable, Category = "Create_Game")
+	static void DestroyRoom(const FString& RoomCode);
+
+	static TSet<FString> ActiveRoomCodes;
+};
